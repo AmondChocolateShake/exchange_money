33cd77d1dbe80c8667a799ed06cd51a5f1960744
AmondChocolateShake <dongju230130@gmail.com>
2023-05-15 14:48:34 +0900

    exchanging is done

--- components/TotalBox.js
+++ components/TotalBox.js
@@ -1,18 +1,41 @@
-import {View} from 'react-native'
+import {View, Text} from 'react-native'
 
 
 const container={
   width:"95%",
   flex:1,
   borderWidth:1,
-  borderColor:"black"
+  borderColor:"black",
 
 }
 
+const innerBox1={
+  flex:1,
+}
+
+const fontSt={
+  fontSize:25,
+  fontWeight:"solid",
+}
+
+const innerBox2={
+  flex:1
+}
+
+
+//총 개수 와 총 값을 표현하는 박스
 const TotalBox=()=>{
   return(
     <View style={container}>
+      <View style={innerBox1}>
+        <Text style={fontSt}>Total : </Text>
+        <Text>{}</Text>
+      </View>
 
+
+      <View style={innerBox2}>
+
+      </View>
     </View>
   );
 }
